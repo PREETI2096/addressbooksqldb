8a838a52aab643b1b71ec37508089f5178537c18
PREETI2096 <preetirg96@gmail.com>
2022-11-25 13:46:40 +0530

    [preeti]:added UC4 edit data in table of addressbookservice DB

--- addressbookservice.sql
+++ addressbookservice.sql
@@ -1,17 +1,21 @@
 CREATE DATABASE addressbookservice;
 USE addressbookservice;
-CREATE TABLE address_book(First_Name varchar(20),
+CREATE TABLE address_book1(
+                    id int auto_increment,
+					First_Name varchar(20),
 				    Last_Name varchar(20),
 				    Address varchar(70),
 				    City varchar(20),
 			 	    State varchar(20),
 			 	    Zip varchar(10),
 				    Phone_Number varchar(20),
-				    Email varchar(40)
-                    );
-INSERT INTO address_book(First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email) VALUES('Preeti', 'Ghatage', 'navalur', 'Dharwad', 'Karnataka', '580009', '8553266354', 'pree12@gmail.com');
-INSERT INTO address_book(First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email) VALUES('Shruti', 'Deshmukh', 'sandur', 'Bellary', 'Andrapradesh', '450092', '7887456129', 'shru@gmail.com');
-INSERT INTO address_book(First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email)VALUES('Ravi', 'Avasankh', 'bmr', 'chennai', 'Tamilnadu', '630789', '8594897269', 'ravi@gmail.com');
-INSERT INTO address_book(First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email)VALUES('Shravana', 'Narasannavar', 'iit', 'pondicherry', 'Tamilnadu', '640159', '8567892139', 'shravz@gmail.com');
-select * from address_book;
-drop table address_book;
+				    Email varchar(40),
+                    primary key(id));
+INSERT INTO address_book1(First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email) VALUES('Preeti', 'Ghatage', 'navalur', 'Dharwad', 'Karnataka', '580009', '8553266354', 'pree12@gmail.com');
+INSERT INTO address_book1(First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email) VALUES('Shruti', 'Deshmukh', 'sandur', 'Bellary', 'Andrapradesh', '450092', '7887456129', 'shru@gmail.com');
+INSERT INTO address_book1(First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email)VALUES('Ravi', 'Avasankh', 'bmr', 'chennai', 'Tamilnadu', '630789', '8594897269', 'ravi@gmail.com');
+INSERT INTO address_book1(First_Name,Last_Name,Address,City,State,Zip,Phone_Number,Email)VALUES('Shravana', 'Narasannavar', 'iit', 'pondicherry', 'Tamilnadu', '640159', '8567892139', 'shravz@gmail.com');
+select * from address_book1;
+drop table address_book1;
+UPDATE address_book1 SET city='Banglore' WHERE First_Name='Shruti';
+select * from address_book1;
