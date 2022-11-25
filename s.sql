e1d3072114eb6b0c21ea8aa97498e337857bc213
PREETI2096 <preetirg96@gmail.com>
2022-11-25 12:06:04 +0530

    [preeti]:added UC1 addressbookservice DB

--- s.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE AddressBookServiceDB;
-CREATE TABLE address_book(First_Name varchar(20),
-				    Last_Name varchar(20),
-				    Address varchar(70),
-				    City varchar(20),
-			 	    State varchar(20),
-			 	    Zip int,
-				    Phone_Number int,
-				    Email varchar(40));
